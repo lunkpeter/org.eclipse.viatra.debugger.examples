0aa34874a620a31b48b186ce11d38bad4379a49d
lunkpeter <lunk.peter@incquerylabs.com>
2016-03-07 14:35:35 +0100

    modified example

--- org.eclipse.incquery.examples.cps.viatradebugger.example/src/org/eclipse/incquery/examples/cps/viatradebugger/CreateInputModels.java
+++ org.eclipse.incquery.examples.cps.viatradebugger.example/src/org/eclipse/incquery/examples/cps/viatradebugger/CreateInputModels.java
@@ -3,19 +3,8 @@ package org.eclipse.incquery.examples.cps.viatradebugger;
 import org.eclipse.incquery.examples.cps.viatradebugger.example.CPSModelInitializer;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(Parameterized.class)
 public class CreateInputModels {
-    
-	@Parameters
-	
-	
-	
-	
-	
 	@Test
     @Ignore
     public void runDebugger() {
